a1a39f69818e1ff2d44187e44f5c5d4800c0c1df
john.arango <johnj42@gmail.com>
2022-12-26 15:22:54 -0500

    Cucumber preprocessor config

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,26 @@
+/**
+ * @type {Cypress.PluginConfig}
+ */
+
+const cucumber = require('cypress-cucumber-preprocessor').default;
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+
 module.exports = {
   "baseUrl": "https://www.demoblaze.com/",
   "testFiles": "**/*.feature",
   "viewportWidth": 1400,
   "viewportHeight": 800,
   "env": {
-      "TAGS": "not @ignore"
+    "TAGS": "not @ignore"
   },
 
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on('file:preprocessor', cucumber())
+      allureWriter(on, config);
+      return config;
     },
+
   },
 };
